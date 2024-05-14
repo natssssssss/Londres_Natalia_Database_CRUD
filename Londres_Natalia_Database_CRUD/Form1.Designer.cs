7697e1af49632977de56cb566fff40abfea84b9c
Natalia Londres <Natalia Londres@NATALIA-LOQ-15I>
2024-05-14 22:54:16 +0800

    Add project files.

---
+++ Londres_Natalia_Database_CRUD/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Londres_Natalia_Database_CRUD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LAST NAME";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(292, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "FIRST NAME";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(51, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(212, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(295, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(212, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(51, 98);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(456, 150);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(51, 282);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(84, 34);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "SUBMIT";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(193, 281);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(91, 35);
+            this.btnUpdate.TabIndex = 6;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(357, 281);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(91, 35);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+    }
+}
+
---
+++ Londres_Natalia_Database_CRUD/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using static Londres_Natalia_Database_CRUD.Form1;
+
+namespace Londres_Natalia_Database_CRUD
+{
+    public partial class Form1 : Form
+    {
+        private DatabaseConnection dbConnection;
+        private DataAccessLayer dataAccessLayer;
+        private String tableName = "students";
+
+        public Form1()
+        {
+            InitializeComponent();
+            dbConnection = new DatabaseConnection("localhost", "adu_sample_db", "root", "");
+            dataAccessLayer = new DataAccessLayer(dbConnection);
+            dataGridView1.SelectionChanged += dataGridView1_SelectionChanged;
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            string query = $"SELECT id, lname AS 'Last Name', fname AS 'First Name' FROM {tableName}";
+            DataTable dataTable = dataAccessLayer.Select(query);
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string lname = textBox1.Text;
+            string fname = textBox2.Text;
+            try
+            {
+                string query = $"INSERT INTO {tableName} (lname, fname) VALUES ('{lname}', '{fname}')";
+                dataAccessLayer.Insert(query);
+                LoadData();
+                MessageBox.Show("Successful");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string lname = textBox1.Text;
+            string fname = textBox2.Text;
+
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                string id = dataGridView1.SelectedRows[0].Cells["id"].Value.ToString();
+                string query = $"UPDATE {tableName} SET lname='{lname}', fname='{fname}' WHERE id={id}";
+                dataAccessLayer.Update(query);
+                LoadData();
+            }
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                string id = dataGridView1.SelectedRows[0].Cells["id"].Value.ToString();
+                string query = $"DELETE FROM {tableName} WHERE id={id}";
+                dataAccessLayer.Delete(query);
+                LoadData();
+
+            }
+        }
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+                textBox1.Text = selectedRow.Cells["Last Name"].Value.ToString();
+                textBox2.Text = selectedRow.Cells["First Name"].Value.ToString();
+            }
+        }
+
+        public class DatabaseConnection
+        {
+            private string connectionString;
+            private MySqlConnection connection;
+
+            public DatabaseConnection(string server, string database, string uid, string password)
+            {
+                connectionString = $"SERVER={server}; DATABASE={database}; UID={uid}; PASSWORD={password};";
+                connection = new MySqlConnection(connectionString);
+            }
+
+            public void OpenConnection()
+            {
+                if (connection.State != ConnectionState.Open)
+                    connection.Open();
+            }
+
+            public void CloseConnection()
+            {
+                if (connection.State != ConnectionState.Closed)
+                    connection.Close();
+            }
+
+            public MySqlConnection GetConnection()
+            {
+                return connection;
+            }
+        }
+
+        public class DataAccessLayer
+        {
+            private DatabaseConnection dbConnection;
+
+            public DataAccessLayer(DatabaseConnection connection)
+            {
+                dbConnection = connection;
+            }
+
+            public DataTable Select(string query)
+            {
+                using (MySqlCommand cmd = new MySqlCommand(query, dbConnection.GetConnection()))
+                {
+                    DataTable dt = new DataTable();
+                    try
+                    {
+                        dbConnection.OpenConnection();
+                        MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+                        adapter.Fill(dt);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error: " + ex.Message);
+                    }
+                    finally
+                    {
+                        dbConnection.CloseConnection();
+                    }
+                    return dt;
+                }
+            }
+
+            public void Insert(string query)
+            {
+                using (MySqlCommand cmd = new MySqlCommand(query, dbConnection.GetConnection()))
+                {
+                    try
+                    {
+                        dbConnection.OpenConnection();
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error: " + ex.Message);
+                    }
+                    finally
+                    {
+                        dbConnection.CloseConnection();
+                    }
+                }
+            }
+
+            public void Update(string query)
+            {
+                using (MySqlCommand cmd = new MySqlCommand(query, dbConnection.GetConnection()))
+                {
+                    try
+                    {
+                        dbConnection.OpenConnection();
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error: " + ex.Message);
+                    }
+                    finally
+                    {
+                        dbConnection.CloseConnection();
+                    }
+                }
+            }
+
+            public void Delete(string query)
+            {
+                using (MySqlCommand cmd = new MySqlCommand(query, dbConnection.GetConnection()))
+                {
+                    try
+                    {
+                        dbConnection.OpenConnection();
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error: " + ex.Message);
+                    }
+                    finally
+                    {
+                        dbConnection.CloseConnection();
+                    }
+                }
+            }
+        }
+    }
+}
